ab7ad9c53b1c7e196f5e0216aa8fa36495f5749e
Anirudha Pagare <127181241+Anirudpagare20@users.noreply.github.com>
2023-11-28 22:38:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const connectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors');
-
-
-connectToMongo();
+const connectToMongo = require('./db');
+const authRoutes = require('./Routes/auth');
+const notesRoutes = require('./Routes/notes');
 
 const app = express();
 const port = 5000;
 
-app.use(cors())
+connectToMongo();
+
+// Use CORS middleware with options to allow requests from localhost
+const corsOptions = {
+  origin: 'http://localhost:3000',  // Replace with your frontend's localhost and port
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use("/api/auth", require("./Routes/auth"));
-app.use("/api/notes", require("./Routes/notes"));
-
+app.use("/api/auth", authRoutes);
+app.use("/api/notes", notesRoutes);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
-
-
